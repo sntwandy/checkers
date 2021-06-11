5557eecdbdc49e5c9275f100db931f95041d0cf2
Wandy Santana <sntwandy@gmail.com>
2021-06-11 09:01:04 -0400

    Board painted

--- js/index.js
+++ js/index.js
@@ -1,19 +1,33 @@
 'use strict'
 
+const square = document.querySelector('#square');
+const container = document.querySelector('#container');
 const board = []
+const paintBoard = []
 
 const createCheckerBoard = () => {
-  for (let i = 0; i < 64; i++) {
-    i % 2 === 0 ?
+  for (let i = 0; i < 8; i++) {
+    let arr = []
+    for (let j = 0; j < 8; j++) {
+      arr.push({
+        id: `${i}${j}`,
+      })
+    }
     board.push({
-      id: i,
-      color: 'white'
-    }) : board.push({
-      id: i,
-      color: 'black'
+      ...arr
     })
   }
 }
 
+const paintCheckerBoard = () => {
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      const node = document.createElement('div')
+      j % 2 === 0 ? node.className = i % 2 == 0 ? 'black square' : 'white square' : node.className = i % 2 !== 0 ? 'black square' : 'white square'
+      container.appendChild(node)
+    }
+  }
+}
+
 createCheckerBoard()
-console.log(board)
+paintCheckerBoard()
